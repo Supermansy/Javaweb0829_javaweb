e679819d0d1715cb5bd86f1ad60df5ffead62ee8
Supermansy <2827362506@qq.com>
2018-08-30 19:10:21 +0800

    +重定向和重新分派

--- src/main/java/com/neuedu/controller/ServletTo.java
+++ src/main/java/com/neuedu/controller/ServletTo.java
@@ -1,10 +1,12 @@
 package com.neuedu.controller;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/Hello")
@@ -27,17 +29,34 @@ public class ServletTo extends HttpServlet {
 
         System.out.println("========doPost请求=======");
 
+
     }
 
+    /**
+     * 请求域 < 会话域 < 全局域
+     * 每个请求域中值私有不共享
+     * 而 会话域和全局域都需要自己手动创建.
+     * 会话域的作用范围是一种浏览器，当服务器不同时，数据就不会共享 创建会话域HttpSession session = request.getSession();
+     * 全局域是跨浏览器是最大的作用域 创建全局域  ServletContext selrvletContext = this.getServletContext();
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        System.out.println("========doGet请求=======");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println(username + "   " +password);
-
+        System.out.println("========doGet请求=======");
+        //赋属性值
+        request.setAttribute("username","admin");
+        //重定向
+        response.sendRedirect("login.jsp");
+        //重新分配
+        request.getRequestDispatcher("login.jsp").forward(request,response);
+        ServletContext selrvletContext = this.getServletContext();
+        HttpSession session = request.getSession();
     }
-
     @Override
     public void destroy() {
         super.destroy();
